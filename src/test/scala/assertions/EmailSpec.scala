fc6d3c8a2a4242b8d3dfb6d75bcc49d8cabd44e3
Grenkin1988 <kruglovgleb@gmail.com>
2021-07-04 13:09:28 +0200

    assertThrows

--- src/test/scala/assertions/EmailSpec.scala
+++ src/test/scala/assertions/EmailSpec.scala
@@ -18,4 +18,32 @@ class EmailSpec extends AnyFlatSpec {
             Email("jim@google.com").localPart
         }
     }
+
+    it should "throw an exception when email does not contain '@' symbol" in {
+        assertThrows[IllegalArgumentException] {
+            Email("jim.com")
+        }
+    }
+
+    it should "throw an exception when email contains more then one '@' symbol" in {
+        assertThrows[IllegalArgumentException] {
+            Email("jim@google@.com")
+        }
+    }
+
+    it should "intercept the correct error message when no '@' symbol is provided" in {
+        val exception = intercept[IllegalArgumentException] {
+            Email("jimgoogle.com")
+        }
+        assert(exception.isInstanceOf[IllegalArgumentException])
+        assert(exception.getMessage.contains("does not contain '@'"))
+    }
+
+    it should "intercept the correct error message when more then one '@' symbol is provided" in {
+        val exception = intercept[IllegalArgumentException] {
+            Email("jim@google@.com")
+        }
+        assert(exception.isInstanceOf[IllegalArgumentException])
+        assert(exception.getMessage.contains("should not contain '@' symbol more than once"))
+    }
 }
